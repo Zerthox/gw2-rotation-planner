1df5bb1fd9c03bd203913dcd7258e69d16f90fd1
Zerthox <zerthox@gmail.com>
2022-04-10 19:37:30 +0200

    Add error handling

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,9 @@
 import {GatsbyNode, Node} from "gatsby";
 import {upperFirst} from "lodash";
 import {fetchSkills} from "./api";
-import {tomePolyfill} from "./src/data";
+import {polyfillSkills} from "./src/data/polyfill";
+
+// TODO: source nodes without gatsby-transformer-yaml to improve data format
 
 interface ProfNode extends Node {
     name: string;
@@ -11,35 +13,34 @@ interface ProfNode extends Node {
 export const onCreateNode: GatsbyNode["onCreateNode"] = async ({node, actions, getNode}) => {
     if (node.internal.type === "DataYaml") {
         const profNode = node as ProfNode;
-
-        // add profession info
         const parent = getNode(profNode.parent);
-        actions.createNodeField({
-            node,
-            name: "profession",
-            value: upperFirst(parent.relativeDirectory as string)
-        });
 
-        if (profNode.name.startsWith("Tome")) {
-            // polyfill tome skill data
+        try {
+            // add profession info
             actions.createNodeField({
                 node,
-                name: "skills",
-                value: profNode.skills.map((id) => tomePolyfill.find((skill) => skill.id === id))
+                name: "profession",
+                value: upperFirst(parent.relativeDirectory as string)
             });
-        } else {
-            // fetch skill data from api
-            try {
-                const skillData = await fetchSkills(profNode.skills);
 
+            if (profNode.name.startsWith("Tome") || profNode.name === "Shadow Shroud") {
+                // polyfill tome data
+                actions.createNodeField({
+                    node,
+                    name: "skills",
+                    value: profNode.skills.map((id) => polyfillSkills.find((skill) => skill.id === id))
+                });
+            } else {
+                // fetch skill data from api
+                const skillData = await fetchSkills(profNode.skills);
                 actions.createNodeField({
                     node,
                     name: "skills",
                     value: skillData.map(({id, name, slot}) => ({id, name, slot}))
                 });
-            } catch (err) {
-                console.error("Failed to fetch skill data:", err);
             }
+        } catch (err) {
+            console.error(`"${parent.absolutePath}":`, err);
         }
     }
 };
