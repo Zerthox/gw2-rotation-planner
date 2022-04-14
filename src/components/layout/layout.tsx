bbcc9189e9a74fd98a1b411e7343c5300d8e228a
Zerthox <zerthox@gmail.com>
2022-04-14 16:54:36 +0200

    Move fonts to assets

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -7,13 +7,9 @@ import {Content} from "./content";
 import {Store, Persistor} from "../../store";
 import {useSiteMeta} from "../../hooks/site";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/source-code-pro";
 import "@discretize/gw2-ui-new/dist/index.css";
 import "@discretize/gw2-ui-new/dist/default_style.css";
+import "../../assets/fonts";
 import "./global.css";
 
 export interface LayoutProps {
