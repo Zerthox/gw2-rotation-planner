10fb9a06c075cf42bf282ebb9e436b123a90c7bb
Zerthox <zerthox@gmail.com>
2022-04-17 15:50:02 +0200

    Add shadow shroud icons

--- src/data/polyfill/shadow-shroud.ts
+++ src/data/polyfill/shadow-shroud.ts
@@ -1,29 +1,45 @@
-import {SkillData, SkillSlot} from "..";
+import {PolyfillSkill} from ".";
+import {SkillSlot} from "..";
 
-export const shadowShroud: SkillData[] = [
+export const shadowShroud: PolyfillSkill[] = [
     {
         id: 63362,
         name: "Haunt Shot",
-        slot: SkillSlot.Weapon1
+        slot: SkillSlot.Weapon1,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/b/be/Haunt_Shot.png"
+        }
     },
     {
         id: 63167,
         name: "Grasping Shadows",
-        slot: SkillSlot.Weapon2
+        slot: SkillSlot.Weapon2,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/e/ef/Grasping_Shadows.png"
+        }
     },
     {
         id: 63220,
         name: "Dawn's Response",
-        slot: SkillSlot.Weapon3
+        slot: SkillSlot.Weapon3,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/3/31/Dawn%27s_Repose.png"
+        }
     },
     {
         id: 63160,
         name: "Eternal Night",
-        slot: SkillSlot.Weapon4
+        slot: SkillSlot.Weapon4,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/7/7b/Eternal_Night.png"
+        }
     },
     {
         id: 63249,
         name: "Mind Shock",
-        slot: SkillSlot.Weapon5
+        slot: SkillSlot.Weapon5,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/e/e6/Mind_Shock.png"
+        }
     }
 ];
