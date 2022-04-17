3fedc43ee485715aeec40c1bafb3683679b84263
Zerthox <zerthox@gmail.com>
2022-04-17 15:49:57 +0200

    Add tome skill icons

--- src/data/polyfill/tomes.ts
+++ src/data/polyfill/tomes.ts
@@ -1,85 +1,131 @@
-import {SkillData, SkillSlot} from "..";
+import {PolyfillSkill} from ".";
+import {SkillSlot} from "..";
 
-export const tomeOfJustice: SkillData[] = [
+export const tomeOfJustice: PolyfillSkill[] = [
     {
         id: 41258,
         name: "Chapter 1: Searing Spell",
-        slot: SkillSlot.Weapon1
+        slot: SkillSlot.Weapon1,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/d/d3/Chapter_1-_Searing_Spell.png"
+        }
     },
     {
         id: 40635,
         name: "Chapter 2: Igniting Burst",
-        slot: SkillSlot.Weapon2
+        slot: SkillSlot.Weapon2,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/5/53/Chapter_2-_Igniting_Burst.png"
+        }
     },
     {
         id: 42449,
         name: "Chapter 3: Heated Rebuke",
-        slot: SkillSlot.Weapon3
+        slot: SkillSlot.Weapon3,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/e/e7/Chapter_3-_Heated_Rebuke.png"
+        }
     },
     {
         id: 40015,
         name: "Chapter 4: Scorched Aftermath",
-        slot: SkillSlot.Weapon4
+        slot: SkillSlot.Weapon4,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/c/c9/Chapter_4-_Scorched_Aftermath.png"
+        }
     },
     {
         id: 42898,
         name: "Epilogue: Ashes of the Just",
-        slot: SkillSlot.Weapon5
+        slot: SkillSlot.Weapon5,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/6/6d/Epilogue-_Ashes_of_the_Just.png"
+        }
     }
 ];
 
-export const tomeOfResolve: SkillData[] = [
+export const tomeOfResolve: PolyfillSkill[] = [
     {
         id: 45022,
         name: "Chapter 1: Desert Bloom",
-        slot: SkillSlot.Weapon1
+        slot: SkillSlot.Weapon1,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/f/fd/Chapter_1-_Desert_Bloom.png"
+        }
     },
     {
         id: 40679,
         name: "Chapter 2: Radiant Recovery",
-        slot: SkillSlot.Weapon2
+        slot: SkillSlot.Weapon2,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/9/95/Chapter_2-_Radiant_Recovery.png"
+        }
     },
     {
         id: 45128,
         name: "Chapter 3: Azure Sun",
-        slot: SkillSlot.Weapon3
+        slot: SkillSlot.Weapon3,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/b/bf/Chapter_3-_Azure_Sun.png"
+        }
     },
     {
         id: 42008,
         name: "Chapter 4: Shining River",
-        slot: SkillSlot.Weapon4
+        slot: SkillSlot.Weapon4,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/1/16/Chapter_4-_Shining_River.png"
+        }
     },
     {
         id: 42925,
         name: "Epilogue: Eternal Oasis",
-        slot: SkillSlot.Weapon5
+        slot: SkillSlot.Weapon5,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/5/5f/Epilogue-_Eternal_Oasis.png"
+        }
     }
 ];
 
-export const tomeOfCourage: SkillData[] = [
+export const tomeOfCourage: PolyfillSkill[] = [
     {
         id: 42986,
         name: "Chapter 1: Unflinching Charge",
-        slot: SkillSlot.Weapon1
+        slot: SkillSlot.Weapon1,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/3/30/Chapter_1-_Unflinching_Charge.png"
+        }
     },
     {
         id: 41968,
         name: "Chapter 2: Daring Challenge",
-        slot: SkillSlot.Weapon2
+        slot: SkillSlot.Weapon2,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/7/79/Chapter_2-_Daring_Challenge.png"
+        }
     },
     {
         id: 41836,
         name: "Chapter 3: Valiant Bulwark",
-        slot: SkillSlot.Weapon3
+        slot: SkillSlot.Weapon3,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/7/73/Chapter_3-_Valiant_Bulwark.png"
+        }
     },
     {
         id: 40988,
         name: "Chapter 4: Stalwart Stand",
-        slot: SkillSlot.Weapon4
+        slot: SkillSlot.Weapon4,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/8/89/Chapter_4-_Stalwart_Stand.png"
+        }
     },
     {
         id: 44455,
         name: "Epilogue: Unbroken Lines",
-        slot: SkillSlot.Weapon5
+        slot: SkillSlot.Weapon5,
+        iconProps: {
+            src: "https://wiki.guildwars2.com/images/d/d8/Epilogue-_Unbroken_Lines.png"
+        }
     }
 ];
