eac8f36537392ab68a89440cf34eec591e1619d7
Zerthox <zerthox@gmail.com>
2022-06-05 18:23:01 +0200

    Hide api & copy id without dev mode

--- src/components/skill/context-menu.tsx
+++ src/components/skill/context-menu.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {OpenInNew, DataObject, DataArray, Fingerprint, PlusOne, Delete} from "@mui/icons-material";
 import {ContextMenu, ContextMenuProps} from "../general";
+import {useDevMode} from "../../store/settings";
 import {copyToClipboard} from "../../util/clipboard";
 
 const wikiUrl = "https://wiki.guildwars2.com";
@@ -14,55 +15,61 @@ export interface SkillContextMenuProps extends ContextMenuProps {
     onDelete?: () => void;
 }
 
-export const SkillContextMenu = ({skill, isCommon, searchValue, onDuplicate, onDelete, ...props}: SkillContextMenuProps): JSX.Element => (
-    <ContextMenu {...props} items={[
-        onDuplicate ? {
-            key: "duplicate",
-            text: "Duplicate Skill",
-            icon: <PlusOne/>,
-            action: () => onDuplicate()
-        } : null,
-        searchValue ? {
-            key: "wiki",
-            text: "Open Wiki",
-            icon: <OpenInNew/>,
-            itemProps: {
-                // TODO: mimic chat code link used by the game
-                href: `${wikiUrl}?title=Special:Search&search=${encodeURIComponent(searchValue)}&go=Go`,
-                target: "_blank",
-                rel: "noopener noreferrer"
-            }
-        } : null,
-        ...!isCommon ? [
-            {
-                key: "api",
-                text: "Open API",
-                icon: <DataObject/>,
+export const SkillContextMenu = ({skill, isCommon, searchValue, onDuplicate, onDelete, ...props}: SkillContextMenuProps): JSX.Element => {
+    const isDev = useDevMode();
+
+    return (
+        <ContextMenu {...props} items={[
+            onDuplicate ? {
+                key: "duplicate",
+                text: "Duplicate Skill",
+                icon: <PlusOne/>,
+                action: () => onDuplicate()
+            } : null,
+            searchValue ? {
+                key: "wiki",
+                text: "Open Wiki",
+                icon: <OpenInNew/>,
                 itemProps: {
-                    href: `${apiUrl}/v2/skills?ids=${skill}&lang=en`,
+                    // TODO: mimic chat code link used by the game
+                    href: `${wikiUrl}?title=Special:Search&search=${encodeURIComponent(searchValue)}&go=Go`,
                     target: "_blank",
                     rel: "noopener noreferrer"
                 }
-            },
-            {
-                key: "id",
-                text: "Copy Skill ID",
-                icon: <Fingerprint/>,
-                action: () => copyToClipboard(skill.toString())
-            },
-            typeof searchValue === "string" ? {
-                key: "chatcode",
-                text: "Copy Chatcode",
-                icon: <DataArray/>,
-                action: () => copyToClipboard(searchValue)
+            } : null,
+            ...!isCommon ? [
+                ...isDev ? [
+                    {
+                        key: "api",
+                        text: "Open API",
+                        icon: <DataObject/>,
+                        itemProps: {
+                            href: `${apiUrl}/v2/skills?ids=${skill}&lang=en`,
+                            target: "_blank",
+                            rel: "noopener noreferrer"
+                        }
+                    },
+                    {
+                        key: "id",
+                        text: "Copy Skill ID",
+                        icon: <Fingerprint/>,
+                        action: () => copyToClipboard(skill.toString())
+                    }
+                ]: [],
+                typeof searchValue === "string" ? {
+                    key: "chatcode",
+                    text: "Copy Chatcode",
+                    icon: <DataArray/>,
+                    action: () => copyToClipboard(searchValue)
+                } : null
+            ] : [],
+            onDelete ? {
+                key: "delete",
+                action: () => onDelete(),
+                text: "Delete Skill",
+                icon: <Delete/>,
+                color: "error.main"
             } : null
-        ] : [],
-        onDelete ? {
-            key: "delete",
-            action: () => onDelete(),
-            text: "Delete Skill",
-            icon: <Delete/>,
-            color: "error.main"
-        } : null
-    ]}/>
-);
+        ]}/>
+    );
+};
