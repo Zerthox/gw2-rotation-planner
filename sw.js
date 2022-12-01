d83cd816cab49aeecf157e81bbee97ac3fe112f6
Zerthox <Zerthox@users.noreply.github.com>
2022-12-01 17:27:56 +0000

    deploy: 2ed845461540c23647e573b152b6f1eedfec4d34

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-967c51f510c52ee0f4a9.js"
+    "url": "webpack-runtime-898832a2b8f6e3a39ab2.js"
   },
   {
     "url": "framework-7614af3981bb910feaff.js"
   },
   {
-    "url": "app-b774119f0149bbae5139.js"
+    "url": "app-9d39769cf459a867596e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1034ebc2c4c8f33452a88590fbeb7032"
+    "revision": "6281804211023e72840a5d709b200334"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -142,12 +142,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
+  pathname = pathname.replace(new RegExp(`^/gw2-rotation-planner`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-b774119f0149bbae5139.js`))) {
+  if (!resources || !(await caches.match(`/gw2-rotation-planner/app-9d39769cf459a867596e.js`))) {
     return await fetch(event.request)
   }
 
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/gw2-rotation-planner/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
